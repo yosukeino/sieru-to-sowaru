30e989206b5829c0ea70883fda42db6cd049a711
yosukeino <yosuke0719@gmail.com>
2023-08-19 10:56:26 +0900

    🔧 reiwa_mov

---
+++ app/components/reiwa_no_tora.js
@@ -0,0 +1,24 @@
+export function Reiwa() {
+  return (
+    <div>
+      <h2 className="">
+        ☆大人気YouTube番組　『令和の虎』に
+        <br />
+        ​塾長が出演しました！
+      </h2>
+      <div class="aspect-w-16 aspect-h-9">
+      <iframe
+  src="https://www.youtube.com/embed/R8b5olq98GM"
+  width="560"
+  height="315"
+  frameborder="0"
+  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  allowfullscreen
+></iframe>
+
+      </div>
+    </div>
+  );
+}
+
+// cookhand
