c0b6b59803eab76d6b35fbd6aa4eaf1cf2a451e9
yosukeino <yosuke0719@gmail.com>
2023-08-23 08:21:13 +0900

    aisatsu

--- app/greeting/page.js
+++ app/greeting/page.js
@@ -1,9 +1,11 @@
+import Aisatsu from "./components/aisatsu";
 import Profile from "./components/profile";
 
 export default function JukuchoAisatsu() {
     return (
       <>
         <Profile />
+        <Aisatsu />
       </>
     );
   }
